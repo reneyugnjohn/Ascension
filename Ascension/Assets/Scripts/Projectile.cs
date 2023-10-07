7e38c0eb201d66875e4bac5e24736c2926a0c319
John Nguyen <johnnguyen1820@gmail.com>
2023-10-07 01:16:31 -0500

    updated sprites
    updated arrow
    updated room tilemap

--- Ascension/Assets/Scripts/Projectile.cs
+++ Ascension/Assets/Scripts/Projectile.cs
@@ -19,7 +19,7 @@ public class Projectile : MonoBehaviour
         Vector3 rotation = transform.position - mousePos;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * speed;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot);
+        transform.rotation = Quaternion.Euler(0, 0, rot + 180);
     }
 
     // Update is called once per frame
