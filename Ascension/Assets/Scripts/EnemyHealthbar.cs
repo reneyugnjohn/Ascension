71cd5d787ff94e4615628765f0dfa2e4ed5905a9
MichaelN <mn04202002@gmail.com>
2023-10-09 17:49:58 -0500

    adjusted enemy healthbar

--- Ascension/Assets/Scripts/EnemyHealthbar.cs
+++ Ascension/Assets/Scripts/EnemyHealthbar.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 public class EnemyHealthbar : MonoBehaviour
 {
     public Slider slider;
+    CanvasGroup canvasGroup;
 
-
+    void Start()
+    {
+        canvasGroup = GetComponent <CanvasGroup>();
+        canvasGroup.alpha = 0;
+    }
     // Update is called once per frame
     void Update()
     {
-
+        if(slider.value < 1)
+        {
+            canvasGroup.alpha = 1;
+        }
     }
 
     public void UpdateHealthbar(float currHealth, float maxHealth)
