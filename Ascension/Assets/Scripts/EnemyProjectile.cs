0ecb7e9bba20908c745cc23596d65c66401f6776
John Nguyen <johnnguyen1820@gmail.com>
2023-10-31 01:30:56 -0500

    implemented crossfade transition state
    now able to crossfade between scenes
    added new "axeman" sprites

--- Ascension/Assets/Scripts/EnemyProjectile.cs
+++ Ascension/Assets/Scripts/EnemyProjectile.cs
@@ -9,16 +9,12 @@ public class EnemyProjectile : MonoBehaviour
     private Rigidbody2D rb;
     public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Fly();
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+        //rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = transform.right * speed;
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -30,12 +26,4 @@ public class EnemyProjectile : MonoBehaviour
         }
         Destroy(gameObject);
     }
-    /*
-    void Fly(Vector3 targetPosition)
-    {
-        rb = GetComponent<Rigidbody2D>(Transform playerTransform);
-        Vector3 direction = playerTransform.position - transform.position;
-        Vector3 rotation = transform.position - playerTransform.position;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * speed;
-    }*/
 }
--- Ascension/Assets/Scripts/MainMenu.cs
+++ Ascension/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime = 2f;
+
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load scene
+        SceneManager.LoadSceneAsync(levelIndex);
     }
 }
