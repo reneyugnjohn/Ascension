547a246b1aee97966c897943bb36e41a2b044346
John Nguyen <johnnguyen1820@gmail.com>
2023-11-01 17:54:44 -0500

    polished option ui
    option open and close animation

---
+++ Ascension/Assets/CloseOnClick.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
+public class CloseOnClick : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    [SerializeField] public float delayTime;
+    
+    public void OpenMenuCloseOptions()
+    {
+        StartCoroutine(Delay(delayTime));
+    }
+
+    IEnumerator Delay(float delayTime)
+    {
+        // Wait
+        yield return new WaitForSeconds(delayTime);
+
+        // Close Options and Load Main Menu
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
