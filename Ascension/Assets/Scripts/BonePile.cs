0a829a978511fb98aebdd4fd92ba6d47484d70b3
John Nguyen <johnnguyen1820@gmail.com>
2023-11-30 16:01:46 -0600

    build settings

--- Ascension/Assets/Scripts/BonePile.cs
+++ Ascension/Assets/Scripts/BonePile.cs
@@ -2,7 +2,6 @@ using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class BonePile : MonoBehaviour
 {
